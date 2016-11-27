bc32504cf22407feb9a4107a154c413a3aa6e303
Vic Lam <lam.vic.k@gmail.com>
2016-11-27 06:38:58 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "too" => "2",
+    "two" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split(" ").collect do |word|
+    dictionary.has_key?(word.downcase) ? word = dictionary[word.downcase] : word = word
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  short = word_substituter(tweet)
+  short.length > 140 ? short[0...140] : short
+end
